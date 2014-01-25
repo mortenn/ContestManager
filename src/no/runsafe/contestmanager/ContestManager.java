5479e764a7142e529965321d374c9c3480b23ebe
Morten Nilsen <morten@runsafe.no>
2014-01-25 13:48:46 +0100

    Initialized new plugin ContestManager from template

---
+++ src/no/runsafe/contestmanager/ContestManager.java
@@ -0,0 +1,26 @@
+package no.runsafe.contestmanager;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class ContestManager extends RunsafePlugin
+//public class ContestManager extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
